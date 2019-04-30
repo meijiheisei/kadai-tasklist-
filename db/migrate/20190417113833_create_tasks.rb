8acf2eb9fb63867308268967844cd2b7c1bdc92a
meiji.heisei <curironakaka@yahoo.co.jp>
2019-04-30 07:59:44 +0000

    add column

--- db/migrate/20190417113833_create_tasks.rb
+++ db/migrate/20190417113833_create_tasks.rb
@@ -1,9 +1,5 @@
 class CreateTasks < ActiveRecord::Migration[5.0]
-  def change
-    create_table :tasks do |t|
-      t.string :content
-
-      t.timestamps
-    end
-  end
-end
+ def change
+    add_column :tasks, :status, :string
+ end
+end  
